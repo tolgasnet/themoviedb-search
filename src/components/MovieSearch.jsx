e807bad689d9e9e76384512d77c3798815157dc0
Tolga Sofuoglu <tolgasofuoglu@gmail.com>
2020-05-08 07:52:46 +0100

    Refactor onChange

--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -1,25 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { searchByTitle } from "../api-clients/movieDBClient";
 
 const MovieSearch = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState([]);
-  const typeDelay = 1000;
 
-  useEffect(() => {
-    const timeOutId = setTimeout(() => {
-      const apiResults = searchByTitle(searchTerm);
-      setResults(apiResults);
-    }, typeDelay);
-    return () => clearTimeout(timeOutId);
-  }, [searchTerm]);
+  const onChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const onClick = () => {
+    const apiResults = searchByTitle(searchTerm);
+    setResults(apiResults);
+  };
 
   return (
     <>
-      <input
-        type="text"
-        onChange={(event) => setSearchTerm(event.target.value)}
-      />
+      <input type="text" onChange={onChange} />
+      <input type="button" value="Search" onClick={onClick} />
       <div>
         {results.map((movie) => (
           <p key={movie.id}>{movie.title}</p>
--- src/components/MovieSearch.test.jsx
+++ src/components/MovieSearch.test.jsx
@@ -2,22 +2,12 @@ import { render, fireEvent } from "@testing-library/react";
 import React from "react";
 import MovieSearch from "./MovieSearch";
 
-jest.useFakeTimers();
-
 describe("movie search test", () => {
-  it("renders a search box", async () => {
-    const { getByRole } = render(<MovieSearch />);
-
-    expect(getByRole("textbox")).toBeTruthy();
-  });
-
   it("displays search results", async () => {
     const { getByRole, getByText } = render(<MovieSearch />);
 
-    const searchBox = getByRole("textbox");
-    fireEvent.change(searchBox, { target: { value: "test-movie" } });
-
-    jest.runAllTimers();
+    fireEvent.change(getByRole("textbox"), { target: { value: "test-movie" } });
+    fireEvent.click(getByRole("button"));
 
     expect(getByText("test-movie-1")).toBeTruthy();
   });
